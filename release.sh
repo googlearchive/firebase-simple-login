9300ecf82d7ef83a5e4061a792fde27a19ac7cdc
jacobawenger <jacob@firebase.com>
2014-07-18 17:26:35 -0700

    Update release.sh script

--- release.sh
+++ release.sh
@@ -5,12 +5,54 @@ DESCRIPTION="Simple Login Web Client"
 STANDALONE_DEST="../firebase-clients/js/simple-login"
 STANDALONE_STUB="firebase-simple-login"
 
-# Ensure the firebase-clients repo is at the correct relative path
-if [[ ! -d $STANDALONE_DEST ]]; then
-  echo "Eror: Destination directory not found; 'firebase-clients' needs to be a sibling of this repo."
+
+#########################################
+#  VALIDATE firebase-simple-login REPO  #
+#########################################
+# Ensure the checked out firebase-simple-login branch is master
+CHECKED_OUT_BRANCH="$(git branch | grep "*" | awk -F ' ' '{print $2}')"
+if [[ $CHECKED_OUT_BRANCH != "master" ]]; then
+  echo "Error: Your firebase-simple-login repo is not on the master branch."
   exit 1
 fi
 
+# Make sure the firebase-simple-login branch does not have existing changes
+if ! git --git-dir=".git" diff --quiet; then
+  echo "Error: Your firebase-simple-login repo has existing changes on the master branch. Make sure you commit and push the new version before running this release script."
+  exit 1
+fi
+
+####################################
+#  VALIDATE firebase-clients REPO  #
+####################################
+# Ensure the firebase-clients repo is at the correct relative path
+if [[ ! -d $STANDALONE_DEST ]]; then
+  echo "Error: The firebase-clients repo needs to be a sibling of this repo."
+  exit 1
+fi
+
+# Go to the firebase-clients repo
+cd ../firebase-clients
+
+# Make sure the checked-out firebase-clients branch is master
+FIREBASE_CLIENTS_BRANCH="$(git branch | grep "*" | awk -F ' ' '{print $2}')"
+if [[ $FIREBASE_CLIENTS_BRANCH != "master" ]]; then
+  echo "Error: Your firebase-clients repo is not on the master branch."
+  exit 1
+fi
+
+# Make sure the firebase-clients branch does not have existing changes
+if ! git --git-dir=".git" diff --quiet; then
+  echo "Error: Your firebase-clients repo has existing changes on the master branch."
+  exit 1
+fi
+
+# Go back to starting directory
+cd -
+
+##############################
+#  VALIDATE CLIENT VERSIONS  #
+##############################
 # Get the version we are releasing
 PARSED_CLIENT_VERSION=$(grep "CLIENT_VERSION" firebase-simple-login-debug.js | head -1 | awk -F '"' '{print $2}')
 
@@ -42,34 +84,68 @@ if [[ $VERSION != $BOWER_VERSION ]]; then
   exit 1
 fi
 
-# Create a new git tag if they have not already done so
+# Ensure there is not an existing git tag for the new version
 LAST_GIT_TAG="$(git tag --list | tail -1 | awk -F 'v' '{print $2}')"
-if [[ $VERSION != $LAST_GIT_TAG ]]; then
-  git tag v$VERSION
-  git push --tags
-
-  echo "*** Last commit tagged as v${VERSION} ***"
-  echo
-else
-  echo "*** Git tag v${VERSION} already created ***"
-  echo
+if [[ $VERSION == $LAST_GIT_TAG ]]; then
+  echo "Error: git tag v${VERSION} already exists. Make sure you are not releasing an already-released version."
+  exit 1
 fi
 
-# Check if we already have this as a standalone
+# Ensure that we don't already have this as a standalone
 STANDALONE_TARGET_DIR="${STANDALONE_DEST}/${VERSION}/"
 if [[ -e ${STANDALONE_TARGET_DIR} ]]; then
   echo "Error: The target directory already exists: ${STANDALONE_TARGET_DIR}."
   exit 1
 fi
 
+
+######################
+#  PUBLISH TO Bower  #
+######################
+# Pull any changes to the geofire repo
+git pull origin master
+if [[ $? -ne 0 ]]; then
+  echo "Error: Failed to do 'git pull' from geofire repo."
+  exit 1
+fi
+
+# Create a git tag for the new version
+git tag v$VERSION
+if [[ $? -ne 0 ]]; then
+  echo "Error: Failed to do 'git tag' from geofire repo."
+  exit 1
+fi
+
+# Push the new git tag
+git push --tags
+if [[ $? -ne 0 ]]; then
+  echo "Error: Failed to do 'git push --tags' from geofire repo."
+  exit 1
+fi
+
+echo "*** Last commit tagged as v${VERSION} ***"
+echo
+
+# Changing the git tag publishes the new version to Bower automatically
+echo "*** v${VERSION} published to Bower ***"
+echo
+
+
+#############################
+#  UPDATE firebase-clients  #
+#############################
 # Make the target directory
-mkdir $STANDALONE_DEST/$VERSION
+mkdir $STANDALONE_TARGET_DIR
+if [[ $? -ne 0 ]]; then
+  echo "Error: Failed to create standalone target directory in firebase-clients repo."
+  exit 1
+fi
 
 # Copy the files to the target directory
 cp $STANDALONE_STUB.js $STANDALONE_TARGET_DIR
 cp $STANDALONE_STUB-debug.js $STANDALONE_TARGET_DIR
 
-echo "*** Client (debug and non-debug) copied ***"
+echo "*** Client (debug and non-debug) files copied ***"
 echo
 
 # Overwrite the existing changelog
@@ -81,38 +157,45 @@ echo
 # Go to the firebase-clients repo
 cd ${STANDALONE_DEST}/
 
-# Make sure the checked-out firebase-clients branch is master
-FIREBASE_CLIENTS_BRANCH="$(git branch | grep "*" | awk -F ' ' '{print $2}')"
-if [[ $FIREBASE_CLIENTS_BRANCH != "master" ]]; then
-  echo "Error: Your firebase-clients repo is not on the master branch. You will need to push the new files to it manually."
-  exit 1
-fi
-
 # Pull any changes to the firebase-clients repo
 git pull origin master
 if [[ $? -ne 0 ]]; then
-  echo "Error pulling firebase-clients repo."
+  echo "Error: Failed to do 'git pull' from firebase-clients repo."
+  exit 1
+fi
+
+# Add the new files to the firebase-clients repo
+git add .
+if [[ $? -ne 0 ]]; then
+  echo "Error: Failed to do 'git add' from firebase-clients repo."
   exit 1
 fi
 
 # Commit to the firebase-clients repo
-git add .
-git commit -am "[firebase-release] Updated Firebase $DESCRIPTION to $VERSION"
+git commit -am "[firebase-release] Updated GeoFire to $VERSION"
+if [[ $? -ne 0 ]]; then
+  echo "Error: Failed to do 'git commit' from firebase-clients repo."
+  exit 1
+fi
 
 # Push the new files to the firebase-clients repo
 git push origin master
 if [[ $? -ne 0 ]]; then
-  echo "Error pushing firebase-clients repo."
+  echo "Error: Failed to do 'git push' to firebase-clients repo."
   exit 1
 fi
 echo
 
-echo "*** Changes pushed to firebase-client ***"
+echo "*** Changes pushed to firebase-clients repo ***"
 echo
 
 # Go back to starting directory
 cd -
 
+
+##################
+#  MANUAL TASKS  #
+##################
 echo
 echo "Manual steps remaining:"
 echo "  1) Deploy firebase-clients to CDN via Jenkins"
